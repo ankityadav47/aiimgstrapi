b635b319ac17c11546535a851a5ca27d9ca1269f
ankityadav47 <ankityadav913156@gmail.com>
2025-03-30 16:53:03 +0530

    Your commit message

--- src/api/cloudinary/routes/cloudinary.js
+++ src/api/cloudinary/routes/cloudinary.js
@@ -1,11 +1,13 @@
 module.exports = {
   routes: [
     {
+
       method: 'POST',
       path: '/cloudinary/upload',
       handler: 'cloudinary.upload',
       config: {
         auth: false
+        
       }
     }
   ]
