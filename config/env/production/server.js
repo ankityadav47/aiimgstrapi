d21633681f447a28a2558d373adba981381984ed
ankityadav47 <ankityadav913156@gmail.com>
2025-03-31 10:41:27 +0530

    fsd

--- config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  url: env('RENDER_EXTERNAL_URL'),
-  app: {
-    keys: env.array('APP_KEYS')
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-}); 
